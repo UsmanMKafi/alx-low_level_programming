1d94d57184abc4852910f57c89230c5f54f990ac
UsmanMKafi <mkafi3000@gmail.com>
2022-12-10 21:41:39 -0800

    C strings manipulation

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - main block
+ *Description: using printf
+ *Return: 0
+ */
+int main (void)
+{
+	printf("with proper grammar, but the output is piece of art, \n");
+
+	return (0);
+}
