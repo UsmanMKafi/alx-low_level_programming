ea7fc5091d9d601a8b13d3caae685fc44781e954
UsmanMKafi <mkafi3000@gmail.com>
2022-12-10 21:47:41 -0800

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
- *main - main block
- *Description: using printf
- *Return: 0
- */
-int main (void)
-{
-	printf("with proper grammar, but the output is piece of art, \n");
+ * main - Entry piont
+ *
+ * Description - Prints string
+ *
+ * Return: Always 0 (Success)
+ **/
 
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
