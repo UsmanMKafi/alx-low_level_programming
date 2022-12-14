9bf220769da585246a62188030cf44a1a94af474
UsmanMKafi <mkafi3000@gmail.com>
2022-12-14 14:21:16 -0800

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	Return(0)
+}
