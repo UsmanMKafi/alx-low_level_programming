766b97b9633b5adb5b57e9134b0348e7802dc791
UsmanMKafi <mkafi3000@gmail.com>
2023-04-23 05:30:52 -0700

    update

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,6 +20,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle)
-	;
-#endif /* MAIN_H */
+char *_strstr(char *haystack, char *needle);
+
+#endif
